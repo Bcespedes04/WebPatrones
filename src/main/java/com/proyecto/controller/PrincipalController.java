df06d3d9f4d0fa29e08a858f7435b4fb02099750
Bcespedes04 <bcespedes@traarepuestos.com>
2024-07-14 16:49:59 -0600

    Segundo Commit Pagina principal

---
+++ src/main/java/com/proyecto/controller/PrincipalController.java
@@ -0,0 +1,14 @@
+package com.proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+
+    @GetMapping("/principal")
+    public String principal(Model model) {
+        return "paginaprincipal"; 
+    }
+}

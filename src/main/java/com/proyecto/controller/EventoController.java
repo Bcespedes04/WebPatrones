4aac72e0387e4f66f300bca172935314140eac78
jose1409 <jaraya40805@ufide.ac.cr>
2024-08-22 19:21:21 -0600

    Update
    FALTA RESISTENCIA Y LA MITAD DE LEVANTAMIENTO DE PESAS

--- src/main/java/com/proyecto/controller/EventoController.java
+++ src/main/java/com/proyecto/controller/EventoController.java
@@ -1,12 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.proyecto.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.proyecto.domain.Evento;
 import com.proyecto.service.EventoService;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,7 +31,6 @@ public class EventoController {
                                                 .filter(maratonClass -> "maraton".equalsIgnoreCase(maratonClass.getNombreEvento()))
                                                 .collect(Collectors.toList());
         model.addAttribute("maratonClasses", maratonClasses);
-        System.out.println("--------------------------------------------------------------------------------"+ maratonClasses);
         return "maraton";
     }
     
@@ -44,7 +45,7 @@ public class EventoController {
     }
     
     @GetMapping("/resistencia")
-    public String aerobicos(Model model) {
+    public String resistencia(Model model) {
         List<Evento> resistenciaClasses = eventoService.getEventos()
                                                    .stream()
                                                    .filter(Evento -> "resistencia".equalsIgnoreCase(Evento.getNombreEvento()))
@@ -53,16 +54,65 @@ public class EventoController {
         return "resistencia";
     }
     
-    @PostMapping("/addmaraton")
-    public String addmaratonClass(@RequestParam("user") int userId,
-                                @RequestParam("nombreEvento") String clase,
+    @PostMapping("/addmaratonClass")
+    public String addmaratonClass(
+                                @RequestParam("nombre_evento") String nombreEvento,
                                 @RequestParam("fecha") String fecha,
                                 @RequestParam("horario") String horario, 
                                 Model model) {
-        return validateAndAddClass(userId, clase, fecha, horario, model, "Zumba");
+        int userId = cargarUserIdDesdeJson(); // Cargar el userId desde el JSON
+        if (fecha == null || fecha.isEmpty()) {
+            model.addAttribute("error", "La fecha es obligatoria.");
+            model.addAttribute("showModal", true);
+            return maraton(model);
+        }
+        if (horario == null || horario.isEmpty()) {
+            model.addAttribute("error", "El horario es obligatorio.");
+            model.addAttribute("showModal", true);
+            return maraton(model);
+        }
+        return validateAndAddClass(userId, nombreEvento, fecha, horario, model, "Maraton");
     }
     
+    @PostMapping("/addlevantamientopesasClass")
+    public String addlevantamientopesasClass(@RequestParam("nombre_evento") String nombreEvento,
+                                @RequestParam("fecha") String fecha,
+                                @RequestParam("horario") String horario, 
+                                Model model) {
+        int userId = cargarUserIdDesdeJson();  // Cargar el userId desde el JSON
+
+        if (fecha == null || fecha.isEmpty()) {
+            model.addAttribute("error", "La fecha es obligatoria.");
+            model.addAttribute("showModal", true);
+            return levantamientopesas(model);
+        }
+        if (horario == null || horario.isEmpty()) {
+            model.addAttribute("error", "El horario es obligatorio.");
+            model.addAttribute("showModal", true);
+            return levantamientopesas(model);
+        }
+        return validateAndAddClass(userId, nombreEvento, fecha, horario, model, "levantamientopesas");
+    }
     
+    @PostMapping("/addresistenciaClass")
+    public String addResistenciaClass(@RequestParam("nombre_evento") String nombreEvento,
+                                @RequestParam("fecha") String fecha,
+                                @RequestParam("horario") String horario, 
+                                Model model) {
+        int userId = cargarUserIdDesdeJson();  // Cargar el userId desde el JSON
+
+        if (fecha == null || fecha.isEmpty()) {
+            model.addAttribute("error", "La fecha es obligatoria.");
+            model.addAttribute("showModal", true);
+            return resistencia(model);
+        }
+        if (horario == null || horario.isEmpty()) {
+            model.addAttribute("error", "El horario es obligatorio.");
+            model.addAttribute("showModal", true);
+            return resistencia(model);
+        }
+        return validateAndAddClass(userId, nombreEvento, fecha, horario, model, "Resistencia");
+    }
     
     private String validateAndAddClass(int userId, String clase, String fecha, String horario, Model model, String expectedClassName) {
         List<Evento> classes = eventoService.getEventos()
@@ -118,29 +168,129 @@ public class EventoController {
         }
     }
     
-    @PostMapping("/deletemaraton")
-    public String deletemaraton(@RequestParam("idEvento") Long idEvento) {
+    @PostMapping("/deleteMaratonClass")
+    public String deletemaraton(@RequestParam("idEvento") int idEvento) {
         eventoService.delete(idEvento);
         return "redirect:/maraton";
     }
 
     @PostMapping("/deletelevantamientopesas")
-    public String deletelevantamientopesas(@RequestParam("idEvento") Long idEvento) {
+    public String deletelevantamientopesas(@RequestParam("id_evento") int idEvento) {
         eventoService.delete(idEvento);
         return "redirect:/levantamientopesas";
     }
 
     @PostMapping("/deleteresistencia")
-    public String deleteresistencia(@RequestParam("idEvento") Long idEvento) {
+    public String deleteresistencia(@RequestParam("id_evento") int idEvento) {
         eventoService.delete(idEvento);
+        return "redirect:/resistencia";
+    }
+    
+    @PostMapping("/modifyMaratonClass")
+    public String modifyMaratonClass(@RequestParam("id_evento") int idEvento,
+                                   @RequestParam("fecha") String fecha,
+                                   @RequestParam("horario") String horario,
+                                   Model model) {
+        Evento maraton = eventoService.getEvento(idEvento);
+        if (maraton != null) {
+            try {
+                maraton.setFecha(Date.valueOf(fecha));
+                maraton.setHorario(convertStringToTime(horario));
+                eventoService.save(maraton);
+            } catch (IllegalArgumentException e) {
+                model.addAttribute("error", "Formato de horario incorrecto. Falta fecha o hora'.");
+                model.addAttribute("showModifyModal", idEvento);
+                model.addAttribute("maratonClasses", eventoService.getEventos()
+                                                                 .stream()
+                                                                 .filter(maratonClass -> "maraton".equalsIgnoreCase(maratonClass.getNombreEvento()))
+                                                                 .collect(Collectors.toList()));
+                return "maraton";
+            }
+        }
+        return "redirect:/maraton";
+    }
+    
+    @PostMapping("/modifylevantamientopesas")
+    public String modifylevantamientopesasClass(@RequestParam("id_evento") int idEvento,
+                                       @RequestParam("fecha") String fecha,
+                                       @RequestParam("horario") String horario,
+                                       Model model) {
+        Evento levantamientopesas = eventoService.getEvento(idEvento);
+        if (levantamientopesas != null) {
+            try {
+                levantamientopesas.setFecha(Date.valueOf(fecha));
+                levantamientopesas.setHorario(convertStringToTime(horario));
+                eventoService.save(levantamientopesas);
+            } catch (IllegalArgumentException e) {
+                model.addAttribute("error", "Formato de horario incorrecto. Falta fecha o hora'.");
+                model.addAttribute("showModifyModal", idEvento);
+                model.addAttribute("levantamientopesasClasses", eventoService.getEventos()
+                                                                   .stream()
+                                                                   .filter(levantamientopesasClass -> "Aerobicos".equalsIgnoreCase(levantamientopesasClass.getNombreEvento()))
+                                                                   .collect(Collectors.toList()));
+                return "resistencia";
+            }
+        }
+        return "redirect:/levantamientopesasClass";
+    }
+    
+    
+    @PostMapping("/modifyresistenciaClass")
+    public String modifyResistenciaClass(@RequestParam("id_evento") int idEvento,
+                                       @RequestParam("fecha") String fecha,
+                                       @RequestParam("horario") String horario,
+                                       Model model) {
+        Evento resistencia = eventoService.getEvento(idEvento);
+        if (resistencia != null) {
+            try {
+                resistencia.setFecha(Date.valueOf(fecha));
+                resistencia.setHorario(convertStringToTime(horario));
+                eventoService.save(resistencia);
+            } catch (IllegalArgumentException e) {
+                model.addAttribute("error", "Formato de horario incorrecto. Falta fecha o hora'.");
+                model.addAttribute("showModifyModal", idEvento);
+                model.addAttribute("resistenciaClasses", eventoService.getEventos()
+                                                                   .stream()
+                                                                   .filter(resistenciaClass -> "Resistencia".equalsIgnoreCase(resistenciaClass.getNombreEvento()))
+                                                                   .collect(Collectors.toList()));
+                return "resistencia";
+            }
+        }
         return "redirect:/resistencia";
     }
     
     
     
-    
-    
-    
-    
+    private Time convertStringToTime(String timeString) {
+        try {
+            if (timeString.length() == 5) {
+                timeString = timeString + ":00";
+            }
+            return Time.valueOf(timeString);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Formato de horario incorrecto. Falta fecha o hora'.");
+        }
+    }
 
+    // Método privado para cargar el userId desde un archivo JSON
+    private int cargarUserIdDesdeJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // Intentar encontrar el archivo en varias ubicaciones
+            File file = new File("src/main/resources/static/userData.json");
+
+            if (!file.exists()) {
+                file = new File("userData.json");  // Intentar en el directorio de trabajo
+            }
+
+            if (file.exists()) {
+                Map<String, Integer> data = objectMapper.readValue(file, Map.class);
+                return data.get("userId");
+            } else {
+                throw new RuntimeException("El archivo userData.json no existe.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el archivo userData.json", e);
+        }
+    }
 }
